c796f8efc8dca18e0d79a78eea7692af3b1f3578
Jess <jessicapeter92@gmail.com>
2016-08-14 16:58:31 -0400

    tests some toggling functions of accordion button

--- app/components/__tests__/AccordionButton-test.js
+++ app/components/__tests__/AccordionButton-test.js
@@ -3,7 +3,7 @@ jest.unmock('../AccordionButton');
 
 
 import React from 'react'; 
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import AccordionButton from '../AccordionButton';
 
 
@@ -14,15 +14,60 @@ describe("Accordion Button", () => {
   var buttonWithFunc = {"value": "ButtonC", "glyphName": "sampleC", "hasFoldOut": false, "func": "someFunc"};
 
 
-  it("renders an AccordionButton element for each variable in the structure array", () => {
-    let wrapper = shallow(
+  it("renders a null-state component when props hasFoldOut==false, and it has no options", () => {
+    let wrapper = mount(
       <div>
-        <AccordionButton 
+        <AccordionButton
           hasFoldOut={false} />
-        <AccordionButton 
-          hasFoldOut={true} />
       </div>
     )
+
+    wrapper.update();
+
+    var noFoldOut = wrapper.find(AccordionButton).get(0);
+    expect(noFoldOut.state).toBe(null);
+
+  })
+
+  it("renders a button that toggles state between open/close when passed hasFoldOut and parentOpen == true", () => {
+    //remember: class is a prop passed to AccordionButton to determine className
+    let wrapper = mount(
+      <div>
+        <AccordionButton
+          parentOpen={true}
+          hasFoldOut={true}
+          class="toggleClicker" />
+      </div>
+    )
+
+    var hasFoldOut = wrapper.find(AccordionButton).get(0);
+    expect(hasFoldOut.state.open).toBe(false);
+
+    var toggleClicker = wrapper.find(AccordionButton).find(".toggleClicker");
+    toggleClicker.simulate("click");
+
+    wrapper.update();
+
+    expect(hasFoldOut.state.open).toBe(true);
+  })
+
+  //note: issues related to unit testing svgs referenced in nodetest relevant here
+    it("renders an svg when passed an options prop", () => {
+    //remember: class is a prop passed to AccordionButton to determine className
+    let wrapper = mount(
+      <div>
+        <AccordionButton
+          parentOpen={true}
+          options={[{"glyphName": "optA"}, {"glyphName": "optB"}]}
+          glyphName="selectoptA"
+          class="accordionButton" />
+      </div>
+    )
+
+    var hasOptions = wrapper.find(AccordionButton).get(0);
+    expect(hasOptions.state.optionsOpen).toBe(false);
+
+    var toggleOptionOpen = wrapper.find("svg");
   })
   
 
